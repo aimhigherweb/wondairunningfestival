46db67901f80037838d8738f70ee00247d4f4fcb
Amy Kapernick <amy@aimhigherweb.design>
2021-04-23 13:58:11 +0800

    updated header details

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="profile" href="http://gmpg.org/xfn/11" />
 
-        <link href="/wp-content/themes/wondairunningfestival/style.css?v=2021-04-23T05:41:43.713Z" rel="stylesheet" />
+        <link href="/wp-content/themes/wondairunningfestival/style.css?v=2" rel="stylesheet" />
         
         <!-- Google Tag Manager -->
         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KJMBWVM');</script>
